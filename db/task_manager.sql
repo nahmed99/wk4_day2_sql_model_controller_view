d3a2b8da1aba058144db416af11573b326d34723
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-11-24 13:25:01 +0000

    created user table sql

--- db/task_manager.sql
+++ db/task_manager.sql
@@ -1,9 +1,17 @@
+-- The order of dropping (existing) tables matters, to prevent referential integrity issues.
 DROP TABLE IF EXISTS tasks;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(255),
+  last_name VARCHAR(255),
+);
 
 CREATE TABLE tasks (
   id SERIAL PRIMARY KEY,
+  user_id INT,
   description VARCHAR(255),
-  assignee VARCHAR(255),
   duration INT,
   completed BOOLEAN
 );
