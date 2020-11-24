abe0ed17dd4560e7810dea5c5e37ee61f22a25f8
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-11-24 13:27:28 +0000

    correction to users table

--- db/task_manager.sql
+++ db/task_manager.sql
@@ -5,12 +5,12 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   first_name VARCHAR(255),
-  last_name VARCHAR(255),
+  last_name VARCHAR(255)
 );
 
 CREATE TABLE tasks (
   id SERIAL PRIMARY KEY,
-  user_id INT,
+  user_id INT REFERENCES users(id),  -- FK (foreign key) to users table.
   description VARCHAR(255),
   duration INT,
   completed BOOLEAN
