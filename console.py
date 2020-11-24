274f55081240d22d276fd08ebdf1f6e87d8556f5
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-11-24 14:15:51 +0000

    create new users in console.py

--- console.py
+++ console.py
@@ -1,9 +1,18 @@
 import pdb
 from models.task import Task
+from models.user import User
 import repositories.task_repository as task_repository
+import repositories.user_repository as user_repository
 
 task_repository.delete_all()
+user_repository.delete_all()
 
+
+jack = User("Jack", "Jarvis")
+user_repository.save(jack)
+
+victor = User("Victor", "McDade")
+user_repository.save(jack)
 
 
 pdb.set_trace()
